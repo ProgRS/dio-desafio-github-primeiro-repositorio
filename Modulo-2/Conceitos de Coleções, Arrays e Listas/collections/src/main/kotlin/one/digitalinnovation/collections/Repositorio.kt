56ea3ec181dc70811442a4531fc4a02b256e4609
ProgRs <lfernandotc@hotmail.com>
2022-06-20 11:39:52 -0300

    Desenvolvimento mutableMapOf

---
+++ Modulo-2/Conceitos de Coleções, Arrays e Listas/collections/src/main/kotlin/one/digitalinnovation/collections/Repositorio.kt
@@ -0,0 +1,17 @@
+package one.digitalinnovation.collections
+
+class Repositorio<T> {
+    private val map = mutableMapOf<String, T>()
+
+    fun create(id: String, value: T){
+       // map.put(id, value)
+        map[id] = value
+    }
+
+    fun remove(id: String) = map.remove(id)
+
+    fun findById(id: String) =  map.get(id) // map[id]
+
+    fun findAll() = map.values
+
+}
---
+++ Modulo-2/Conceitos de Coleções, Arrays e Listas/collections/src/main/kotlin/one/digitalinnovation/collections/TesteMutavelMap.kt
@@ -0,0 +1,24 @@
+package one.digitalinnovation.collections
+
+fun main() {
+
+    val joao = Funcionario("Joao", 200.00, "CLT")
+    val pedro = Funcionario("Pedro", 1500.00, "PJ")
+    val maria = Funcionario("Maria", 4000.00, "CLT")
+
+    val repositorio = Repositorio<Funcionario>()
+
+    repositorio.create(joao.nome, joao)
+    repositorio.create(pedro.nome, pedro)
+    repositorio.create(maria.nome, maria)
+
+    println(repositorio.findById(joao.nome))
+
+    println("------------")
+    repositorio.findAll().forEach { println(it) }
+
+    println("------------")
+    repositorio.remove(maria.nome)
+    repositorio.findAll().forEach { println(it) }
+
+}
